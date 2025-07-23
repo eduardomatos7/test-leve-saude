c6bec33cb2c30d73e1e134a8d68f984d8e6e2b5b
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-23 10:42:56 -0300

    style: define color constants for styling

---
+++ test-mobile-leve-saude/constants/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary: "#ff3400",
+  secondary: "#451e44",
+  label: "#878787",
+  text: "#404040",
+  background: "#f4f4f4",
+  boxShadow: "1px 2px 6px rgba(0, 0, 0, 0.25)",
+  shadow: {
+    primary: "0px 5px 10px rgba(0, 0, 0, 0.25)",
+  },
+};
