f9d6b7b6118a3b58ded6f4d5d5e8e4b95ad2156b
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-24 02:28:56 -0300

    chore: update Button and Colors components for visual improvements

--- test-mobile-leve-saude/components/Button.tsx
+++ test-mobile-leve-saude/components/Button.tsx
@@ -1,35 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ViewStyle,
+} from "react-native";
 import { Colors } from "../constants/Colors";
 
 function Button({
   handleSubmit,
   text,
+  loading,
 }: {
   handleSubmit: () => void;
   text: string;
+  loading?: boolean;
 }) {
   return (
-    <TouchableOpacity style={style.button} onPress={handleSubmit}>
-      <Text style={style.buttonText}>{text}</Text>
+    <TouchableOpacity
+      disabled={loading ? true : false}
+      style={loading ? style.buttonLoading : style.button}
+      onPress={handleSubmit}
+    >
+      {loading ? (
+        <ActivityIndicator size="small" color={Colors.background} />
+      ) : (
+        <Text style={style.buttonText}>{text}</Text>
+      )}
     </TouchableOpacity>
   );
 }
-
+const baseButton: ViewStyle = {
+  width: 200,
+  height: 50,
+  borderRadius: 8,
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: 8,
+  backgroundColor: Colors.secondary,
+};
 const style = StyleSheet.create({
   button: {
-    width: 150,
-    height: 48,
-    backgroundColor: Colors.secondary,
-    borderRadius: 8,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 8,
+    ...baseButton,
   },
   buttonText: {
-    color: "#fff",
+    color: Colors.white,
     fontSize: 16,
     fontWeight: "bold",
+  },
+  buttonLoading: {
+    ...baseButton,
+    opacity: 0.7,
   },
 });
 
