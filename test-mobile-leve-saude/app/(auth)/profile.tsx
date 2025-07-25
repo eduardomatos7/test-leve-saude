71c62cf7bde9476997bd0f05f5585ea75eef0abe
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-25 19:01:08 -0300

    feat: create logout/Account screen and refactor

--- test-mobile-leve-saude/app/(auth)/profile.tsx
+++ test-mobile-leve-saude/app/(auth)/profile.tsx
@@ -1,19 +1,46 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { getAuth, signOut } from "@react-native-firebase/auth";
 import React from "react";
-import { View } from "react-native";
-import Button from "../../components/Home/Button";
+import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
+import { Colors } from "../../constants/Colors";
+import style from "../../styles/authStyles/styles";
+
+const logo = require("../../assets/images/logo_leve_saude.png");
 
 function Home() {
+  function handleSignOut() {
+    Alert.alert("Fazer Logout", "Tem certeza que deseja sair da conta?", [
+      {
+        text: "Cancelar",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => signOut(getAuth()).then(() => alert("Você saiu!")),
+      },
+    ]);
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button
-        strokColor
-        width={60}
-        text="Sair"
-        onChange={() => {
-          signOut(getAuth()).then(() => alert("Você saiu!"));
-        }}
-      />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <View style={style.logoContainer}>
+        <Image source={logo} style={style.logo} />
+      </View>
+      <TouchableOpacity style={style.buttonBackground} onPress={handleSignOut}>
+        <MaterialIcons name="logout" size={24} color={Colors.white} />
+      </TouchableOpacity>
+      <Text style={{ fontSize: 18, fontWeight: 900, color: Colors.logout }}>
+        {" "}
+        Sair da conta
+      </Text>
     </View>
   );
 }
