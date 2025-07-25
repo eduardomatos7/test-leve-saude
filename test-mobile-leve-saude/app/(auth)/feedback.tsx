0ac0aac2af248507ec6f8696bba212cd42601e52
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-25 03:45:09 -0300

    feat: create home screen and user account for logout and add routes

--- test-mobile-leve-saude/app/(auth)/feedback.tsx
+++ test-mobile-leve-saude/app/(auth)/feedback.tsx
@@ -1,24 +1,39 @@
-import { getAuth, signOut } from "@react-native-firebase/auth";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-import Button from "../../components/Button";
+import { Image, Text, View } from "react-native";
+import Button from "../../components/Home/Button";
+import style from "../../styles/feedback.styles";
+
+const logo = require("../../assets/images/logo_leve_saude.png");
 
 function Feedback() {
+  const router = useRouter();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Feedback authenticated</Text>
-      <Button
-        handleSubmit={async () => {
-          try {
-            await signOut(getAuth());
-            alert("Você saiu!");
-          } catch (error) {
-            alert("Erro ao sair da conta!");
-            console.log(error);
-          }
-        }}
-        text="Logout Test"
-      />
+      <View style={style.logoContainer}>
+        <Image source={logo} style={style.logo} />
+      </View>
+      <View style={style.container}>
+        <Text style={style.title}>Bem vindo ao Leve Saúde!</Text>
+        <Text style={style.subtitle}>
+          Aqui, você pode compartilhar seus feedbacks e acessar todas as suas
+          avaliações feitas, ajudando-nos a oferecer sempre o melhor cuidado.
+        </Text>
+      </View>
+      <View style={style.btnSection}>
+        <Button
+          text="Avaliar"
+          onChange={() => router.push("/(modals)/rate")}
+          backgroundColor
+          width={37}
+        />
+        <Button
+          text="Ver Avaliações"
+          width={60}
+          strokColor
+          onChange={() => router.push("/(modals)/myRate")}
+        />
+      </View>
     </View>
   );
 }
--- test-mobile-leve-saude/app/(auth)/profile.tsx
+++ test-mobile-leve-saude/app/(auth)/profile.tsx
@@ -1,10 +1,19 @@
+import { getAuth, signOut } from "@react-native-firebase/auth";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Button from "../../components/Home/Button";
 
 function Home() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile</Text>
+      <Button
+        strokColor
+        width={60}
+        text="Sair"
+        onChange={() => {
+          signOut(getAuth()).then(() => alert("Você saiu!"));
+        }}
+      />
     </View>
   );
 }
