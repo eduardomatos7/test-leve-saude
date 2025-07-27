0db0bce50e3e5158a2f8ddb7a5e4b6af449d9d4e
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-27 02:00:18 -0300

    feat: add validation with zod and react hook form

--- test-web-leve-saude/src/components/form/inputField.tsx
+++ test-web-leve-saude/src/components/form/inputField.tsx
@@ -1,13 +1,22 @@
 import type { ReactNode } from "react";
+import type { FieldError } from "react-hook-form";
 
 interface InputFieldProps {
   id: string;
   type: string;
   placeholder: string;
   icon: ReactNode;
+  error?: FieldError;
 }
 
-export default function InputField({ id, type, placeholder, icon }: InputFieldProps) {
+export default function InputField({
+  id,
+  type,
+  placeholder,
+  icon,
+  error,
+  ...rest
+}: InputFieldProps) {
   return (
     <div className="mb-5">
       <label htmlFor={id} className="mb-1 block font-medium text-gray-500">
@@ -21,10 +30,14 @@ export default function InputField({ id, type, placeholder, icon }: InputFieldPr
           id={id}
           type={type}
           placeholder={placeholder}
-          className="w-full rounded-lg border-none bg-white py-2 pr-3 pl-10 text-sm shadow-sm focus:outline-none md:text-base"
           required
+          className="w-full rounded-lg border-none bg-white py-2 pr-3 pl-10 text-sm shadow-sm focus:outline-none md:text-base"
+          {...rest}
         />
       </div>
+      <span className="mt-[-60px]">
+        {error?.message && <span className="text-error text-sm">{error?.message}</span>}
+      </span>
     </div>
   );
 }
