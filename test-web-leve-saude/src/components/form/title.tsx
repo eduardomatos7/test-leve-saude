cf2aea39796f5252583c0200331ba89bb4a66b4b
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-27 22:14:40 -0300

    fix: installs dependencies and fixes files

--- test-web-leve-saude/src/components/form/title.tsx
+++ test-web-leve-saude/src/components/form/title.tsx
@@ -1,6 +1,6 @@
 function Title() {
   return (
-    <h2 className="text-primary mb-8 text-center text-xl font-bold italic md:text-2xl">
+    <h2 className="text-primary mb-10 text-center text-xl font-bold italic md:text-2xl">
       Faça Login para prosseguir!
     </h2>
   );
